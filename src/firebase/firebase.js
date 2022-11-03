4052a26f30b1d650d2b7173fe94b21f72d389da0
pnichols789 <pnichols789@gmail.com>
2022-11-03 13:11:15 -0500

    -

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDnasYpZSByCTTVYjzudA-c73OLkSdWnaA",
-  authDomain: "contact-app-9791f.firebaseapp.com",
-  projectId: "contact-app-9791f",
-  storageBucket: "contact-app-9791f.appspot.com",
-  messagingSenderId: "67337289819",
-  appId: "1:67337289819:web:4f31f028711df70c2f3f7e"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messageSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
